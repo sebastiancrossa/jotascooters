b7680f38af62d4e0129ae542e1cf6322df0834b7
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-26 20:48:20 -0700

    Added smooth scrolling for anchor links

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,3 +1,15 @@
+// Site JS
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 // Scroll Reveal
 
 window.sr = ScrollReveal();
